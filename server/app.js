eecc73c4599bb474b7bd70c0f429b1265f48e4bf
XCH <xcheladrian@gmail.com>
2024-05-29 06:01:07 -0600

    Commit message

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const authRoutes = require('./routes/authRoutes');
 const whatsappRoutes = require('./routes/whatsappRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SSL_PORT = 3443;
+
+// Configura el uso de certificados SSL
+const sslOptions = {
+    key: fs.readFileSync('./server/ApiWhatsapp/ssl/selfsigned.key'),
+    cert: fs.readFileSync('./server/ApiWhatsapp/ssl/selfsigned.crt')
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,4 +31,9 @@ app.use('/api/whatsapp', whatsappRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+// Crear un servidor HTTPS
+https.createServer(sslOptions, app).listen(SSL_PORT, () => {
+    console.log(`HTTPS Server running on port ${SSL_PORT}`);
 });
