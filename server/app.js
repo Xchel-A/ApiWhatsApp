b27096bdfe6da8e41bb5ffb2be79973209ce12ea
XCH <xcheladrian@gmail.com>
2024-05-29 17:08:03 -0600

    Commit message

--- server/app.js
+++ server/app.js
@@ -15,9 +15,17 @@ const privateKey = fs.readFileSync('/etc/letsencrypt/live/dendenmushi.space/priv
 const certificate = fs.readFileSync('/etc/letsencrypt/live/dendenmushi.space/fullchain.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
-// Configura CORS para permitir solo el dominio dendemushi.com.mx
+// Configura CORS para permitir solo los dominios dendemushi.com.mx y whatsapi.dendenmushi.com.mx
+const allowedOrigins = ['https://dendemushi.com.mx', 'https://whatsapi.dendenmushi.com.mx'];
+
 const corsOptions = {
-    origin: 'https://dendemushi.com.mx',
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
