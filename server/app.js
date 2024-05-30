3f2eb4e864da77116a892915ddd226332a2ba9a6
XCH <xcheladrian@gmail.com>
2024-05-30 03:19:07 -0600

    Commit message

--- server/app.js
+++ server/app.js
@@ -15,22 +15,8 @@ const privateKey = fs.readFileSync('/etc/letsencrypt/live/dendenmushi.space/priv
 const certificate = fs.readFileSync('/etc/letsencrypt/live/dendenmushi.space/fullchain.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
-// Configura CORS para permitir solo los dominios dendemushi.com.mx y whatsapi.dendenmushi.com.mx
-const allowedOrigins = ['https://dendemushi.com.mx', 'https://whatsapi.dendenmushi.com.mx', 'https://tuservidorphp.com'];
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions)); // Utiliza las opciones de CORS ajustadas
+app.use(cors()); // Utiliza las opciones de CORS ajustadas
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
