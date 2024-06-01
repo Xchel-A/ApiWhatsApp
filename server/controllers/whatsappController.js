288fc0e3d11c6180f6ef8b4511138e3b7c7b26a1
XCH <xcheladrian@gmail.com>
2024-05-31 19:39:04 -0600

    initial commit

--- server/controllers/whatsappController.js
+++ server/controllers/whatsappController.js
@@ -1,6 +1,5 @@
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
-const axios = require('axios'); // Asegúrate de importar axios
 
 const clients = {};
 
@@ -48,31 +47,9 @@ const initializeClient = (userId) => {
             client.destroy();  // Ensure client instance is destroyed
             delete clients[userId];  // Remove client from the clients object
         });
-        client.on('message', async msg => {
-            if (msg.body) {
-                const userId = client.options.authStrategy.clientId;
-
-                try {
-                    // Llamada a la API de contexto del cliente
-                    //const contextResponse = await axios.post('https://api.tucontexto.com/context', { userId }, { timeout: 10000 });
-                    const context = 'Comportate como un asistente virtual';
-
-                    // Inicializar sesión de ChatGPT
-                    await axios.post('https://dendenmushi.space:3001/init', { token: userId }, { timeout: 200000 });
-
-                    // Enviar el contexto como primer mensaje a ChatGPT
-                    await axios.post('https://dendenmushi.space:3001/chat', { token: userId, message: context }, { timeout: 200000 });
-
-                    // Enviar el mensaje recibido por el cliente a ChatGPT
-                    const chatResponse = await axios.post('https://dendenmushi.space:3001/chat', { token: userId, message: msg.body }, { timeout: 200000 });
-                    const replyMessage = chatResponse.data.response;
-
-                    // Responder al cliente con el mensaje recibido de ChatGPT
-                    msg.reply(replyMessage);
-                } catch (error) {
-                    console.error(`Error processing message for user ${userId}:`, error.message);
-                    msg.reply('Lo siento, hubo un error al procesar tu mensaje.');
-                }
+        client.on('message', msg => {
+            if (msg.body == '!ping') {
+                msg.reply('pong');
             }
         });
 
