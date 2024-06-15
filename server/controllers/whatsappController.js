3d3cb0869a431ca89cb0d48483809b9c72fee38e
XCH <xcheladrian@gmail.com>
2024-06-15 05:52:56 -0600

    M

--- server/controllers/whatsappController.js
+++ server/controllers/whatsappController.js
@@ -311,7 +311,7 @@ const logout = async (req, res) => {
             res.status(200).json({ message: 'Client logged out successfully.' });
         } catch (error) {
             console.error('Error logging out:', error);
-            res.status  (500).json({ message: 'Error logging out', error });
+            res.status(500).json({ message: 'Error logging out', error });
         }
     } else {
         res.status(400).json({ message: 'Client not initialized' });
