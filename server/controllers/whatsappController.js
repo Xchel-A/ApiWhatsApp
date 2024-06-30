1de36e9132eb13df44a7b9c53c0120e5fee5ffea
XCH <xcheladrian@gmail.com>
2024-06-30 04:14:00 -0600

    m

--- server/controllers/whatsappController.js
+++ server/controllers/whatsappController.js
@@ -23,7 +23,7 @@ const initializeClient = async (token) => {
         authStrategy: new LocalAuth({ clientId: token }),
         puppeteer: {
             headless: true,
-            args: ['--sandbox', '--disable-gpu'],
+            args: ['--no-sandbox', '--disable-gpu'],
         },
         webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html' }
     });
